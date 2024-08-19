635c532e809ef5ff2136b19710db4fd81fea5782
rianolhh <lhh964513@gmail.com>
2024-08-19 16:46:54 +0900

    1

---
+++ day04/react01_Todo/src/ItemRow.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const ItemRow = ({item, onDoneFlag, onDelete, onEdit})=>{
+    const [flag, setFlag] = useState(false);
+    const [outputTitle, setOutputTtile] = useState(item.title);
+    const lineThroughClass = {textDecoration:"line-through", color:"blue"}
+    return(<div className="input-group mb-3">
+        <div className="input-group-prepend">
+        <div className="input-group-text">
+        <input onChange={()=>{
+            onDoneFlag(item);
+        }} checked={item.done&&"checked"} type="checkbox" />
+        </div>
+        </div>
+        <input 
+            style={ item.done?lineThroughClass:{}} 
+            type="text" className="form-control" 
+            readOnly={flag?"":"readOnly"} 
+            value={outputTitle}
+            onChange={(e)=>{
+                setOutputTtile(e.target.value);
+            }}
+            onFocus={(e)=>{
+                setFlag(true);
+            }}
+            onBlur={(e)=>{
+                console.log("blur");
+                setOutputTtile(item.title);
+            }}
+            />
+            <div className="input-group-append">
+                <button onClick={()=>{
+                    //edit 버튼을 클릭하면 blur가 먼저 호출
+                    onEdit({no:item.no, title:outputTitle, done:item.done});
+                }} className="btn btn-primary" type="button">Edit</button>  
+                <button onClick={()=>{
+                    onDelete(item);
+                }} className="btn btn-danger" 
+                type="button">Delete</button>  
+        </div>
+    </div>);
+}
+
+export default ItemRow;
