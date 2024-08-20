5e2414144718d69148c4535ec8d15dd40897db58
rianolhh <lhh964513@gmail.com>
2024-08-20 13:16:10 +0900

    1

--- day04/react02_Todo2/src/Output.js
+++ day04/react02_Todo2/src/Output.js
@@ -3,31 +3,29 @@ import ItemRow from "./ItemRow";
 const Output = ({onEdit, onDelete, onDoneFlag, todoList}) => {
     return (
         <div className="list-body">
-            <div className="container">
-                <table className="table table-hover">
-                    <thead>
-                    <tr style={{textAlign: "center"}}>
-                        <th>Done</th>
-                        <th>Title</th>
-                        <th>Buttons</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {todoList.map((item) => {
-                        return (<tr key={item.no}>
+        <div className="container">
+            <table className="table table-hover">
+            <thead>
+                <tr style={{textAlign: "center"}}>
+                    <th>Done</th>
+                    <th>Title</th>
+                    <th>Buttons</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {todoList.map((item) => {
+                    return (
+                        <tr key={item.no}>
                             <td colSpan={3} style={{padding: "0px"}}>
                                 <ItemRow item={item} onDoneFlag={onDoneFlag} onDelete={onDelete} onEdit={onEdit}/>
                             </td>
                         </tr>)
-                    })}
-                    </tbody>
-                </table>
-                <ul>
-
-                </ul>
-            </div>
+                })}
+            </tbody>
+            </table>
         </div>
-
+        </div>
     )
 }
 
